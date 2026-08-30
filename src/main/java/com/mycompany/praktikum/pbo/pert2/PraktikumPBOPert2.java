c5551fb3984124084a14eab56d3a184ad4f672bd
Zhio4ffarel <4ffarel@gmail.com>
2026-08-30 15:45:44 +0700

    Tugas modul 1 - praktikum java

---
+++ src/main/java/com/mycompany/praktikum/pbo/pert2/PraktikumPBOPert2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.praktikum.pbo.pert2;
+
+/**
+ *
+ * @author zhioh
+ */
+public class PraktikumPBOPert2 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama  : Muhammad Zhio Affarel");
+        System.out.println("NPM   : 2505060063");
+        System.out.println("Alamat: Tidar Baru");
+        System.out.println("No_HP : 085156120909");
+    }
+}
